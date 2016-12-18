335aac8e121ad95d0c4c4edd3e2e94b12f175995
Nik Muhammad Amin <nik.muhammad.amin@gmail.com>
2016-12-18 08:33:24 +0800

    Fix issue saving score. Clean up code.

--- app/src/main/java/com/crespro/myamaltutor/Kira2/SenaraiAmal2.java
+++ app/src/main/java/com/crespro/myamaltutor/Kira2/SenaraiAmal2.java
@@ -3,7 +3,6 @@ package com.crespro.myamaltutor.Kira2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -14,8 +13,6 @@ import com.crespro.myamaltutor.database.DatabaseService;
 import com.crespro.myamaltutor.model.Amal;
 
 import io.realm.Realm;
-import io.realm.RealmList;
-import io.realm.RealmResults;
 
 public class SenaraiAmal2 extends AppCompatActivity {
 
@@ -81,16 +78,11 @@ public class SenaraiAmal2 extends AppCompatActivity {
                 makeCalculations();
 
             }
-
         });
-
-
     }
 
     private void makeCalculations() {
-
         float sum = 0;
-
 
         if (((CheckBox) findViewById(R.id.checkBox_jemaah2)).isChecked()) sum += 1;
         if (((CheckBox) findViewById(R.id.checkBox_quran2)).isChecked()) sum += 1;
@@ -107,54 +99,12 @@ public class SenaraiAmal2 extends AppCompatActivity {
         mTotal2Btn.setText(String.format("Prestasi Amal Anda Minggu ini adalah  : %.2f", +sum / 10 * 100));
 
         Amal amal2 = new Amal();
-        amal2.setLevel2(1);
-        amal2.setWeek2(1); // 1 is for week one
-        amal2.setScore2(sum / 10 * 100); // score value
+        amal2.setLevel(2); // 2 is for level 2
+        amal2.setWeek(1); // 1 is for week 1g
+        amal2.setScore(sum / 10 * 100); // score value
         DatabaseService.getInstance().copyOrUpdateAmal(mRealm2, amal2); // save amal into database
 
-        // to get back the amal from database
-        getAmal();
-        getAmalByWeek();
-        getAmals();
-
         mScoreMingguan2Btn.setVisibility(View.VISIBLE);
-    }
-
-    /**
-     * Get amal from first row of the database
-     */
-    private void getAmal() {
-        Amal amal2 = DatabaseService.getInstance().getAmal(mRealm2);
-        Log.d("AMAL LOG", "MINGGU: " + amal2.getWeek2());
-        Log.d("AMAL LOG", "SKOR: " + amal2.getScore2());
-    }
-
-    /**
-     * Get amal by week. You can change the week based on you need and get the result (score)
-     * that belongs to that particular week
-     */
-    private void getAmalByWeek() {
-        int week2 = 1;
-        Amal amal2 = DatabaseService.getInstance().getAmalByWeek(mRealm2, week2);
-        Log.d("AMAL LOG", "MINGGU: " + amal2.getWeek2());
-        Log.d("AMAL LOG", "SKOR: %.2f" + amal2.getScore2());
-
-    }
-
-    /**
-     * Get all amals from database. It will iterate based on you amal database size.
-     */
-    private void getAmals() {
-        RealmResults<Amal> amalList2 = DatabaseService.getInstance().getAmals(mRealm2);
-        RealmList<Amal> amals2 = new RealmList<>();
-        for (Amal amal2 : amalList2) {
-            amals2.add(amal2);
-        }
-
-        for (int i = 1; i < amals2.size(); i++) {
-            Log.d("AMAL LOG", "MINGGU: " + amals2.get(i).getWeek2());
-            Log.d("AMAL LOG", "SKOR: %.2f" + amals2.get(i).getScore2());
-        }
     }
 
     @Override
